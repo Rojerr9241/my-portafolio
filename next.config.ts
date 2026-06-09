2a9c2b53a065d816537388f9cf9c1d79e299452c
Rodrigo Negrete <95aezzro@gmail.com>
2026-06-08 20:02:22 -0600

    Adding Shadcn

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', /* Configure Next.js to generate HTML/CSS/JS (SSG) files. */
   reactCompiler: true,
 };
 
